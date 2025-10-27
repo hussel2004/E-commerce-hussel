81232967a40bcddd2cf568d56ff575788dc61ec9
ArreyntowKerone <1enowarreyntow@gmail.com>
2025-10-27 01:25:20 +0100

    applied JS to homepage and resolved some issues with other pages

---
+++ Scripts/featuredproducts.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',() => {
+        document.querySelectorAll('[data-href]').forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            const href = el.dataset.href; //To get the data from the element and store its value
+            if (!href) return;
+            // if you want to open in same tab:
+            window.location.href = href;
+            // or to replace history entry:
+            // window.location.replace(href);
+            // or open in new tab:
+            // window.open(href, '_blank');
+        });
+    });
+});
