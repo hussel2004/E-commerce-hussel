3e4c1dc0d109f1bdceb7eeca3b493dca55f91266
ArreyntowKerone <1enowarreyntow@gmail.com>
2025-11-12 15:55:47 +0100

    Added the footer and header to all pages and translated some pages to English

--- Scripts/panier.js
+++ Scripts/panier.js
@@ -86,3 +86,21 @@ document.addEventListener("DOMContentLoaded", () => {
   setupCheckoutButton();
   updateCartTotal();
 });
+
+// Continue shopping
+document.addEventListener('DOMContentLoaded', () => {
+    // simple navigation for elements that use data-href
+    document.querySelectorAll('[data-href]').forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            const href = el.dataset.href; //To get the data from the element and store its value
+            if (!href) return;
+            // if you want to open in same tab:
+            window.location.href = href;
+            // or to replace history entry:
+            // window.location.replace(href);
+            // or open in new tab:
+            // window.open(href, '_blank');
+        });
+    });
+});
